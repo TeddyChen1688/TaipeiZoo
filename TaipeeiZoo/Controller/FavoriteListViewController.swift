cbb872ef2622bc0371be7302e553a1dc2ae93504
TeddyChen1688 <teddy1014@gmail.com>
2018-06-01 10:39:33 +0800

    add TableView Basic

--- TaipeeiZoo/Controller/FavoriteListViewController.swift
+++ TaipeeiZoo/Controller/FavoriteListViewController.swift
@@ -7,13 +7,63 @@
 //
 
 import UIKit
+import CoreData
 
-class FavoriteListViewController: UITableViewController {
+class FavoriteListViewController: UITableViewController, NSFetchedResultsControllerDelegate{
+    
+    // MARK: - Properties
+    
+    var favorites: [FavoriteMO] = []
+    
+    @IBOutlet var emptyRestaurantView: UIView!
+    
+    var fetchResultController: NSFetchedResultsController<FavoriteMO>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+//
+//
+//        // Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+
+         navigationController?.hidesBarsOnSwipe = true
+//
+//        // Prepare the empty view
+        tableView.backgroundView = emptyRestaurantView
+        tableView.backgroundView?.isHidden = true
+//
+//        // Fetch data from data store
+        let fetchRequest: NSFetchRequest<FavoriteMO> = FavoriteMO.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+            let context = appDelegate.persistentContainer.viewContext
+            fetchResultController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+            fetchResultController.delegate = self
+
+            do {
+                try fetchResultController.performFetch()
+                if let fetchedObjects = fetchResultController.fetchedObjects {
+                    favorites = fetchedObjects
+                }
+            } catch {
+                print(error)
+            }
+        }
 
         
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnSwipe = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,68 +71,45 @@ class FavoriteListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
+    // MARK: - UITableViewDataSource Protocol
 
-   
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if favorites.count > 0 {
+            tableView.backgroundView?.isHidden = true
+            tableView.separatorStyle = .singleLine
+        } else {
+            tableView.backgroundView?.isHidden = false
+            tableView.separatorStyle = .none
+        }
+        
+        return 1
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return favorites.count
     }
 
    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell", for: indexPath)
 
         // Configure the cell...
 
+        let cellIdentifier = "FavoriteCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! FavoriteCell
+        
+        // Configure the cell...
+        cell.nameLabel.text = favorites[indexPath.row].name
+        if let favoritesImage = favorites[indexPath.row].image {
+            cell.thumbnailImageView.image = UIImage(data: favoritesImage as Data)
+        }
+
+        cell.heartImageView.isHidden = favorites[indexPath.row].isVisited ? false : true
+        
         return cell
     }
    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
