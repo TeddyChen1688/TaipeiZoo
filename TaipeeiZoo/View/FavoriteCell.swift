3436ab64f41d47450cf01ceade266c844b3feda5
TeddyChen1688 <teddy1014@gmail.com>
2018-06-01 09:59:57 +0800

    add MyFavorite template

---
+++ TaipeeiZoo/View/FavoriteCell.swift
@@ -0,0 +1,38 @@
+//
+//  FavoriteCell.swift
+//  TaipeiZoo
+//
+//  Created by eva on 2018/6/1.
+//  Copyright © 2018年 Teddy Chen. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteCell: UITableViewCell {
+    
+    
+    @IBOutlet var nameLabel: UILabel! {
+        didSet {
+            nameLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet {
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.width / 2
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+    @IBOutlet var heartImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
